abec0a12249976364d9883c932763c0ba4620f70
Bobby Spencer <bbdgtl@gmail.com>
2017-11-06 17:43:17 +0100

    add scroll to top functionality in projects

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -14,23 +14,24 @@
    ga('create', 'UA-105438021-1', 'auto');
    ga('send', 'pageview');
 
-   // Analytics END
 
-   //   // When the user scrolls down 20px from the top of the document, show the button
-   //   window.onscroll = function () {
-   //       scrollFunction()
-   //   };
-   //
-   //   function scrollFunction() {
-   //       if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
-   //           document.getElementById("myBtn").style.display = "block";
-   //       } else {
-   //           document.getElementById("myBtn").style.display = "none";
-   //       }
-   //   }
-   //
-   //   // When the user clicks on the button, scroll to the top of the document
-   //   function topFunction() {
-   //       document.body.scrollTop = 0; // For Chrome, Safari and Opera 
-   //       document.documentElement.scrollTop = 0; // For IE and Firefox
-   //   }
+   jQuery(document).ready(function () {
+       var offset = 250;
+       var duration = 300;
+       jQuery(window).scroll(function () {
+           if (jQuery(this).scrollTop() > offset) {
+               jQuery('.back - to - top').fadeIn(duration);
+           } else {
+               jQuery('.back - to - top').fadeOut(duration);
+           }
+       });
+
+
+       jQuery('.back - to - top').click(function (event) {
+           event.preventDefault();
+           jQuery('html, body').animate({
+               scrollTop: 0
+           }, duration);
+           return false;
+       })
+   });
